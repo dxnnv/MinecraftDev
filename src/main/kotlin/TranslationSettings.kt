a1e5c36b74ac3c0cb341489e6aa60eb71ece949d
danny <danny@atropos.cc>
2025-08-02 00:48:38 -0400

    refactor: only using single properties file

--- src/main/kotlin/TranslationSettings.kt
+++ src/main/kotlin/TranslationSettings.kt
@@ -21,11 +21,13 @@
 package com.demonwav.mcdev
 
 import com.intellij.openapi.components.PersistentStateComponent
+import com.intellij.openapi.components.Service
 import com.intellij.openapi.components.State
 import com.intellij.openapi.components.Storage
 import com.intellij.openapi.components.service
 import com.intellij.openapi.project.Project
 
+@Service(Service.Level.PROJECT)
 @State(name = "TranslationSettings", storages = [Storage("minecraft_dev.xml")])
 class TranslationSettings : PersistentStateComponent<TranslationSettings.State> {
 
