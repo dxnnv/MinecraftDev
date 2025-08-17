90f441c41f27ad7c61acf9efd1289469613dfb4d
danny <danny@atropos.cc>
2025-08-16 20:48:02 -0400

    style: remove redundant suppression

--- src/main/kotlin/asset/GeneralAssets.kt
+++ src/main/kotlin/asset/GeneralAssets.kt
@@ -20,10 +20,8 @@
 
 package com.demonwav.mcdev.asset
 
-@Suppress("unused")
 object GeneralAssets : Assets() {
     val LISTENER = loadIcon("/assets/icons/general/EventListener_dark.png")
     val PLUGIN = loadIcon("/assets/icons/general/plugin.png")
-
     val MC_TEMPLATE = loadIcon("/assets/icons/general/MinecraftTemplate.png")
 }
